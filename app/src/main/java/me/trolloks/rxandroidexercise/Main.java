a6d3c6db4d0967a80f79c4256a017fb2be59cb63
Rikus Louw <rikus@blazingchilli.com>
2017-06-15 09:42:33 +0200

    first commit

---
+++ app/src/main/java/me/trolloks/rxandroidexercise/Main.java
@@ -0,0 +1,19 @@
+package me.trolloks.rxandroidexercise;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by rikus on 2017/06/15.
+ */
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+}
